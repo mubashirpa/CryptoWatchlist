725c82b606a51c3358a4d65c5cc73688f062b419
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-21 15:05:38 +0530

    feat: Add CoinEntity and mappers for local data persistence
    This commit introduces `CoinEntity` for local data persistence and adds mapper functions to convert between `CoinDto`, `CoinEntity`, and `Coin` domain models. These additions enable the app to store and retrieve coin data locally.

--- app/src/main/java/com/example/cryptowatchlist/data/mapper/CoinMapper.kt
+++ app/src/main/java/com/example/cryptowatchlist/data/mapper/CoinMapper.kt
@@ -1,5 +1,6 @@
 package com.example.cryptowatchlist.data.mapper
 
+import com.example.cryptowatchlist.data.local.entity.CoinEntity
 import com.example.cryptowatchlist.data.remote.dto.CoinAssetsDto
 import com.example.cryptowatchlist.data.remote.dto.CoinDto
 import com.example.cryptowatchlist.domain.model.Coin
@@ -7,6 +8,40 @@ import com.example.cryptowatchlist.domain.model.Coin
 fun CoinAssetsDto.toCoinDomainModelList(): List<Coin> = `data`?.map { it.toCoinDomainModel() } ?: emptyList()
 
 fun CoinDto.toCoinDomainModel(): Coin =
+    Coin(
+        changePercent24Hr = changePercent24Hr,
+        explorer = explorer,
+        id = id,
+        marketCapUsd = marketCapUsd,
+        maxSupply = maxSupply,
+        name = name,
+        priceUsd = priceUsd,
+        rank = rank,
+        supply = supply,
+        symbol = symbol,
+        volumeUsd24Hr = volumeUsd24Hr,
+        vwap24Hr = vwap24Hr,
+    )
+
+fun CoinAssetsDto.toCoinEntityList(): List<CoinEntity> = `data`?.map { it.toCoinEntity() } ?: emptyList()
+
+fun CoinDto.toCoinEntity(): CoinEntity =
+    CoinEntity(
+        id = id!!,
+        changePercent24Hr = changePercent24Hr,
+        explorer = explorer,
+        marketCapUsd = marketCapUsd,
+        maxSupply = maxSupply,
+        name = name,
+        priceUsd = priceUsd,
+        rank = rank,
+        supply = supply,
+        symbol = symbol,
+        volumeUsd24Hr = volumeUsd24Hr,
+        vwap24Hr = vwap24Hr,
+    )
+
+fun CoinEntity.toCoinDomainModel(): Coin =
     Coin(
         changePercent24Hr = changePercent24Hr,
         explorer = explorer,
