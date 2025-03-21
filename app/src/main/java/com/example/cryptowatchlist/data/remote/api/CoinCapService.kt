79b2c09396e2e500e7fd2c2e64e42107bbb50713
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-21 20:34:17 +0530

    feat: Add Authorization header and encoding to CoinCap API
    This commit adds an `Authorization` header to the `getAssets` API call in the `CoinCapService`. It also adds `Accept-Encoding: deflate` to the header.

--- app/src/main/java/com/example/cryptowatchlist/data/remote/api/CoinCapService.kt
+++ app/src/main/java/com/example/cryptowatchlist/data/remote/api/CoinCapService.kt
@@ -2,11 +2,15 @@ package com.example.cryptowatchlist.data.remote.api
 
 import com.example.cryptowatchlist.data.remote.dto.CoinAssetsDto
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface CoinCapService {
+    @Headers("Accept-Encoding: deflate")
     @GET("v2/assets")
     suspend fun getAssets(
+        @Header("Authorization") token: String,
         @Query("search") search: String?,
         @Query("ids") ids: List<String>?,
         @Query("limit") limit: Int?,
