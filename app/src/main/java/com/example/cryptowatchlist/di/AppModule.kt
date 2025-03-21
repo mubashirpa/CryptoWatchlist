8631554020e8322cdc3eae2c7a7ce9f01a655ccf
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-21 13:01:26 +0530

    feat: Set up CoinCap API integration with Koin
    This commit configures the application to interact with the CoinCap API. It includes setting up Retrofit for API calls, binding the CoinCap service, creating the repository, and providing the use case via Koin.

--- app/src/main/java/com/example/cryptowatchlist/di/AppModule.kt
+++ app/src/main/java/com/example/cryptowatchlist/di/AppModule.kt
@@ -1,7 +1,26 @@
 package com.example.cryptowatchlist.di
 
+import com.example.cryptowatchlist.core.Constants
+import com.example.cryptowatchlist.data.remote.api.CoinCapService
+import com.example.cryptowatchlist.data.repository.CoinAssetsRepositoryImpl
+import com.example.cryptowatchlist.domain.repository.CoinAssetsRepository
+import com.example.cryptowatchlist.domain.usecase.GetCoinAssetsUseCase
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule =
     module {
+        single {
+            Retrofit
+                .Builder()
+                .baseUrl(Constants.COIN_CAP_API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(CoinCapService::class.java)
+        }
+        singleOf(::CoinAssetsRepositoryImpl) { bind<CoinAssetsRepository>() }
+        single { GetCoinAssetsUseCase(get()) }
     }
