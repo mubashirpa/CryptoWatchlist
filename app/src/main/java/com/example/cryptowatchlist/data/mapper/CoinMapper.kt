32b17197e5afd8e543b3e75f0b25551db707ed4e
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-21 13:00:16 +0530

    feat: Add Coin model and mapper
    This commit introduces a new `Coin` data model for representing cryptocurrency information and a `CoinMapper` for transforming `CoinDto` objects from the remote data source into `Coin` domain model objects.

---
+++ app/src/main/java/com/example/cryptowatchlist/data/mapper/CoinMapper.kt
@@ -0,0 +1,23 @@
+package com.example.cryptowatchlist.data.mapper
+
+import com.example.cryptowatchlist.data.remote.dto.CoinAssetsDto
+import com.example.cryptowatchlist.data.remote.dto.CoinDto
+import com.example.cryptowatchlist.domain.model.Coin
+
+fun CoinAssetsDto.toCoinDomainModelList(): List<Coin> = `data`?.map { it.toCoinDomainModel() } ?: emptyList()
+
+fun CoinDto.toCoinDomainModel(): Coin =
+    Coin(
+        changePercent24Hr = changePercent24Hr,
+        explorer = explorer,
+        id = id,
+        marketCapUsd = marketCapUsd,
+        maxSupply = maxSupply,
+        name = name,
+        priceUsd = priceUsd,
+        rank = rank,
+        supply = supply,
+        symbol = symbol,
+        volumeUsd24Hr = volumeUsd24Hr,
+        vwap24Hr = vwap24Hr,
+    )
---
+++ app/src/main/java/com/example/cryptowatchlist/domain/model/Coin.kt
@@ -0,0 +1,16 @@
+package com.example.cryptowatchlist.domain.model
+
+data class Coin(
+    val changePercent24Hr: String? = null,
+    val explorer: String? = null,
+    val id: String? = null,
+    val marketCapUsd: String? = null,
+    val maxSupply: String? = null,
+    val name: String? = null,
+    val priceUsd: String? = null,
+    val rank: String? = null,
+    val supply: String? = null,
+    val symbol: String? = null,
+    val volumeUsd24Hr: String? = null,
+    val vwap24Hr: String? = null,
+)
