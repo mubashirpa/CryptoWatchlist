d5d177311f152deb53cfedc46ddc2e863d6bdf3b
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-21 11:58:44 +0530

    Refactor: Relocate and update MainActivity and associated files
    This commit moves the `MainActivity` class to a new `presentation.main` package.
    It also includes the following specific changes:
    
    -   Deletes the original `MainActivity.kt` file.
    -   Creates a new `MainActivity.kt` in the `presentation.main` package.
    -   Updates the `AndroidManifest.xml` to reflect the new location of `MainActivity`.
    -   Adds dynamic colors support to the new `MainActivity`.
    -   Updates `activity_main.xml` to align with the relocated `MainActivity`.
    -   Adds splash screen to the `MainActivity`.
    -   Adds `ActivityMainBinding` to `MainActivity` for the binding of the view.

---
+++ app/src/main/java/com/example/cryptowatchlist/presentation/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.cryptowatchlist.presentation.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.cryptowatchlist.databinding.ActivityMainBinding
+import com.google.android.material.color.DynamicColors
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+
+        DynamicColors.applyToActivityIfAvailable(this)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
