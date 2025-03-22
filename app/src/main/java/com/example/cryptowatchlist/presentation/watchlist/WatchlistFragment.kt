9ac3daaff18992a38d8a9434de64a231b7134277
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-22 16:50:28 +0530

    feat: Implement watchlist UI with coin list display
    This commit implements the watchlist UI, including the display of a list of coins.

--- app/src/main/java/com/example/cryptowatchlist/presentation/watchlist/WatchlistFragment.kt
+++ app/src/main/java/com/example/cryptowatchlist/presentation/watchlist/WatchlistFragment.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cryptowatchlist.databinding.FragmentWatchlistBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class WatchlistFragment : Fragment() {
     private var _binding: FragmentWatchlistBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: WatchlistViewModel by viewModel()
+    private val navController by lazy { findNavController() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +33,34 @@ class WatchlistFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.recyclerView) { v, insets ->
+            val bars =
+                insets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
+            v.updatePadding(
+                left = bars.left,
+                right = bars.right,
+                bottom = bars.bottom,
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+
+        val adapter =
+            WatchlistAdapter(
+                onClickListener =
+                    WatchlistAdapter.OnClickListener {
+                        // TODO: Navigate to coin details screen
+                    },
+            )
+        binding.recyclerView.adapter = adapter
+
+        viewModel.watchlist.observe(viewLifecycleOwner) { watchlist ->
+            adapter.submitList(watchlist)
+        }
+
+        binding.topAppBar.setNavigationOnClickListener {
+            navController.navigateUp()
+        }
     }
 
     override fun onDestroyView() {
