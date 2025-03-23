a142c5148467d53dc83810deb30b5b6877c0cb2d
Mubashir P A <mubashirpa2002@gmail.com>
2025-03-23 12:24:15 +0530

    feat: Implement GetWatchlistUseCase with dynamic price updates
    This commit refactors the `GetWatchlistUseCase` to include dynamic price updates for the coins in the watchlist. It retrieves the latest price data from the CoinCap API and updates the local database with the new values. Additionally, it introduces a flag to ensure that the price update is only triggered once per data retrieval.

--- app/src/main/java/com/example/cryptowatchlist/domain/usecase/GetWatchlistUseCase.kt
+++ app/src/main/java/com/example/cryptowatchlist/domain/usecase/GetWatchlistUseCase.kt
@@ -1,16 +1,34 @@
 package com.example.cryptowatchlist.domain.usecase
 
+import com.example.cryptowatchlist.BuildConfig
 import com.example.cryptowatchlist.data.mapper.toCoinDomainModel
+import com.example.cryptowatchlist.data.mapper.toCoinEntityList
 import com.example.cryptowatchlist.domain.model.Coin
 import com.example.cryptowatchlist.domain.repository.CoinAssetsRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.flow
 
 class GetWatchlistUseCase(
     private val repository: CoinAssetsRepository,
 ) {
+    var priceUpdated = false
+
     operator fun invoke(): Flow<List<Coin>> =
-        repository.getCoins().map {
-            it.map { it.toCoinDomainModel() }
+        flow {
+            try {
+                repository.getCoins().collect {
+                    emit(it.map { it.toCoinDomainModel() })
+                    if (!priceUpdated) {
+                        val token = BuildConfig.COIN_CAP_API_KEY
+                        val ids = it.joinToString(",") { it.id }
+                        val latest =
+                            repository.getCoinAssets(token = token, ids = ids).toCoinEntityList()
+                        repository.insertCoins(latest)
+                        priceUpdated = true
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
 }
